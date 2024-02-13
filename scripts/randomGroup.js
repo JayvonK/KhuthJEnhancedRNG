95dc37239076dd315cfa02da813e4cba253764a1
JayvonK <jaykhuth@gmail.com>
2024-02-13 12:51:58 -0800

    done

--- scripts/randomGroup.js
+++ scripts/randomGroup.js
@@ -1,7 +1,10 @@
+import { generateDiv } from "./app.js";
+
 const randomGroup = (arr, num) => {
     if(num !== "0") {
         let array = [];
         let randArr = arr;
+        let count = 0;
 
         for (let i = 0; i < num; i++) {
             let empty = [];
@@ -17,7 +20,29 @@ const randomGroup = (arr, num) => {
                 }
             }
         }
-        console.log(array);
+
+        generateDiv.innerHTML = "";
+
+        array.map(ray => {
+            count++;
+            let h2 = document.createElement("h2");
+            h2.textContent = "Group " + count + ": ";
+            h2.className = "light groups";
+
+            for(let i = 0; i < ray.length; i++){
+                if(ray.length > 1){
+                    if(i === ray.length - 1){
+                    h2.textContent += ray[i];
+                    }else {
+                       h2.textContent += ray[i] + ", "; 
+                    }
+                } else {
+                    h2.textContent += ray[i];
+                }
+            }
+
+            generateDiv.append(h2);
+        })
     } 
 }
 
